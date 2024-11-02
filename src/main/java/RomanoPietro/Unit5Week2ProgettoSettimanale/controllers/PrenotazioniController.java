959f1ebfaaf4f984c92e385ada355baf94d41b6a
Pietroro98 <pietroro98@gmail.com>
2024-11-02 14:06:26 +0100

    aggiunta dello screen del diagramma er che mi ero dimenticato

--- src/main/java/RomanoPietro/Unit5Week2ProgettoSettimanale/controllers/PrenotazioniController.java
+++ src/main/java/RomanoPietro/Unit5Week2ProgettoSettimanale/controllers/PrenotazioniController.java
@@ -1,4 +1,12 @@
 package RomanoPietro.Unit5Week2ProgettoSettimanale.controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prenotazioni")
 public class PrenotazioniController {
+
+
 }
